4b0303e1451ed65864f459cfa738a2eb53217a43
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-02-03 15:08:13 -0800

    added in const to js file

--- workerTracker.js
+++ workerTracker.js
@@ -2,3 +2,11 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: 'Protomansql5',
+    database: 'worker_trackDB'
+});
+
