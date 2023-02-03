ff2020f0a7bf0bd4b0c1525a2555377431591bd6
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-02-03 14:50:53 -0800

    added in starting code insert into within workertracker file

--- workerTracker.sql
+++ workerTracker.sql
@@ -1,1 +1,23 @@
 USE employee_trackDB;
+
+INSERT INTO department (name)
+VALUES ('Engineering'), ('Finance'), ('Sales'), ('Legal');
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jonathan", "Smith", 1, 1),
+("Beau", "Miller", 1, 1),
+("Jeremiah", "Kolder",3, NULL),
+()
+()
+()
+()
+();
+
+INSERT INTO role (title, salary, department_id)
+VALUES ()
+()
+()
+()
+()
+()
+();
