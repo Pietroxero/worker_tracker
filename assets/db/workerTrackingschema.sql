b1066e8af69734c233565ed0fc631299c3a90bda
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-02-03 14:15:00 -0800

    added in starter tables in schema

--- assets/db/workerTrackingschema.sql
+++ assets/db/workerTrackingschema.sql
@@ -3,3 +3,20 @@
 DROP DATABASE IF EXISTS worker_trackDB;
 Create DATABASE worker_trackDB;
 USE worker_trackDB;
+
+--this will be the creation of the tables
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(30),
+    PRIMARY KEY(id)
+); 
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id NOT NULL,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id),
+    PRIMARY KEY (id)
+);
