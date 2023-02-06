bbc7dfddea0e1c9c9009fd75ac7dbf98fe058687
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-02-05 17:18:37 -0800

    added in js code for questionaire prompt

--- workerTracker.js
+++ workerTracker.js
@@ -10,3 +10,60 @@ const connection = mysql.createConnection({
     database: 'worker_trackDB'
 });
 
+connection.connect((err) => {
+if (err) {
+    console.error(`could not connect: ${err.stack}`);
+    return;
+}
+//call back const that will prompt the user with questions
+questionaire(); 
+});
+
+//here is where we build the questions a user will be prompted.
+const questionaire = () => {
+    inquirer.prompt([{
+        name: 'qA',
+        type: 'list',
+        message: 'What are ya gonna do?',
+        choices: ['Display Departments', 'Display Roles', 'Display Personnel', 
+        'Display specific Department', 'Display specific Role',
+    'Display specific Employee', 'Display specific Employee Role'],
+    }]).then((answer) => {
+        switch(answer.qA){
+            case 'Display Departments':
+            displayDepartments();
+            break;
+
+            case 'Display Roles':
+                displayRoles();
+                break;
+
+                case 'Display Personnel':
+                    displayPersonnel();
+                    break;
+
+                    case 'Display specific Department':
+                    displayspecificDepartment();
+                    break;
+
+                    case 'Display specific Role':
+                        displayspecificRole();
+                        break;
+
+                        case 'Display specific Employee':
+                            displayspecificEmployee();
+                            break;
+
+                            case 'Display specific Employee Role':
+                                displayspecificemployeeRole();
+                                break;
+
+                                default:
+                                    break;
+        }
+    })
+}
+
+const displayDepartments = () => {
+
+};
